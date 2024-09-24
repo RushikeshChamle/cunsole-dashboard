c1e14cc80b6eae9db37da4b135f1c1361ae81920
Rushikesh Chamle <rushikeshchamle23@gmail.com>
2024-09-25 01:11:43 +0530

    added edit Customer & Email triggers

--- src/app/(hydrogen)/actions/page.tsx
+++ src/app/(hydrogen)/actions/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 const pageHeader = {
   title: 'Email Triggers List',
@@ -17,7 +17,8 @@ const pageHeader = {
   ],
 };
 
-
+import { Drawer, Text, Input, Textarea } from 'rizzui';
+import axiosInstance from '@/axiosInstance'; // Adjust your import for axiosInstance
 
 import Link from 'next/link';
 import { routes } from '@/config/routes';
@@ -26,8 +27,9 @@ import PageHeader from '@/app/shared/page-header';
 import ExportButton from '@/app/shared/export-button';
 import { metaObject } from '@/config/site.config';
 import { useEffect, useState } from 'react';
-import { Badge, Table } from "rizzui";
+import { Badge, Table } from 'rizzui';
 import { PiPlusBold } from 'react-icons/pi';
+import Footer from '@/app/multi-step/footer';
 
 // Define TypeScript types for the API response
 interface EmailTrigger {
@@ -58,13 +60,15 @@ function getConditionTypeLabel(type: number): string {
   }
 }
 
-
-
 export default function EmailTriggersListPage() {
   const [emailTriggers, setEmailTriggers] = useState<EmailTrigger[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-
+ 
+  const [drawerState, setDrawerState] = useState({
+    isOpen: false,
+    triggerDetails: null as EmailTrigger | null, // Store trigger details here
+  });
 
 
   useEffect(() => {
@@ -72,39 +76,53 @@ export default function EmailTriggersListPage() {
       setLoading(true);
       setError(null);
       try {
-        const token = getCookie('access_token');
-  
-        if (!token) {
-          setError('No access token found');
-          return;
-        }
-  
-        const response = await fetch('http://localhost:9000/customers/get_email_triggers/', {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-        });
-  
-        if (!response.ok) {
-          const errorData = await response.json();
-          throw new Error(errorData.error || 'Network response was not ok');
-        }
-
-        // Extract the email_triggers array from the response
-        const data = await response.json();
-        setEmailTriggers(data.email_triggers); // Update here
-        console.log(data)
-      } catch (error: any) {
-        setError(error.message);
+        const response = await axiosInstance.get(
+          '/customers/get_email_triggers/'
+        );
+        setEmailTriggers(response.data.email_triggers); // Adjust to match your response
+      } catch (error) {
+        setError('Failed to fetch email triggers');
       } finally {
         setLoading(false);
       }
     }
-  
     fetchData();
   }, []);
+
   
+  const fetchEmailTriggerById = async (triggerId: string) => {
+    try {
+      const response = await axiosInstance.get(
+        `/customers/email_trigger/${triggerId}`
+      );
+      setDrawerState({
+        isOpen: true, // Set isOpen to true to open the drawer
+        triggerDetails: response.data, // Store the fetched details
+        isEditable: false, // Open in view-only mode initially
+      });
+    } catch (error) {
+      console.error('Failed to fetch email trigger:', error);
+    }
+  };
+
+  // Toggle edit mode for the drawer
+  const handleEditToggle = () => {
+    setDrawerState((prevState) => ({
+      ...prevState,
+      isEditable: !prevState.isEditable,
+    }));
+  };
+
+  // Save the edited details (stubbed function for now)
+  const handleSaveChanges = async () => {
+    // Implement save logic for updating the email trigger
+    console.log('Saving changes...');
+    // Close the edit mode after saving
+    setDrawerState((prevState) => ({
+      ...prevState,
+      isEditable: false,
+    }));
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading data: {error}</p>;
@@ -127,42 +145,191 @@ export default function EmailTriggersListPage() {
         </div>
       </PageHeader>
 
+      <Drawer
+        isOpen={drawerState.isOpen} // Ensure state is controlling drawer
+        size="lg"
+        onClose={() =>
+          setDrawerState((prevState) => ({ ...prevState, isOpen: false }))
+        } // Close drawer handler
+      >
+        <div className="flex h-full flex-col">
+          <div className="flex-1 overflow-y-auto px-5 py-2">
+            <h3 className="mb-4 text-xl font-semibold">
+              Email Trigger Details
+            </h3>
+
+            {drawerState.triggerDetails && (
+              <div className="space-y-4">
+                {/* Input fields for viewing/editing details */}
+                <Input
+                  label="Name"
+                  value={drawerState.triggerDetails.name}
+                  disabled={!drawerState.isEditable}
+                  onChange={(e) =>
+                    setDrawerState((prevState) => ({
+                      ...prevState,
+                      triggerDetails: {
+                        ...prevState.triggerDetails!,
+                        name: e.target.value,
+                      },
+                    }))
+                  }
+                />
+                <Input
+                  label="Condition Type"
+                  value={getConditionTypeLabel(
+                    drawerState.triggerDetails.condition_type
+                  )}
+                  disabled
+                />
+                <Textarea
+                  label="Email Subject"
+                  value={drawerState.triggerDetails.email_subject}
+                  disabled={!drawerState.isEditable}
+                  onChange={(e) =>
+                    setDrawerState((prevState) => ({
+                      ...prevState,
+                      triggerDetails: {
+                        ...prevState.triggerDetails!,
+                        email_subject: e.target.value,
+                      },
+                    }))
+                  }
+                />
+                <Textarea
+                  label="Email Body"
+                  value={drawerState.triggerDetails.email_body}
+                  disabled={!drawerState.isEditable}
+                  onChange={(e) =>
+                    setDrawerState((prevState) => ({
+                      ...prevState,
+                      triggerDetails: {
+                        ...prevState.triggerDetails!,
+                        email_body: e.target.value,
+                      },
+                    }))
+                  }
+                />
+                <Input
+                  label="Days Offset"
+                  value={drawerState.triggerDetails.days_offset.toString()}
+                  disabled={!drawerState.isEditable}
+                  onChange={(e) =>
+                    setDrawerState((prevState) => ({
+                      ...prevState,
+                      triggerDetails: {
+                        ...prevState.triggerDetails!,
+                        days_offset: parseInt(e.target.value, 10),
+                      },
+                    }))
+                  }
+                />
+              </div>
+            )}
+          </div>
+
+          {/* Footer with Action buttons */}
+          <div
+            className="mt-2 px-5"
+            style={{
+              position: 'relative',
+              // top: "-6px",
+              top: 'calc(-14px + 2vh);' /* Adjusts the top position dynamically based on viewport height */,
+            }}
+          >
+            {' '}
+            {/* Optional: Adjust margin-top if needed */}
+            <div className="flex justify-end space-x-1">
+              {drawerState.isEditable ? (
+                <>
+                  <Button onClick={handleEditToggle} variant="outline">
+                    Cancel
+                  </Button>
+                  <div className="w-2" />{' '}
+                  {/* Gap between Cancel and Save buttons */}
+                  <Button onClick={handleSaveChanges}>Save</Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    variant="outline"
+                    onClick={() =>
+                      setDrawerState({
+                        isOpen: false,
+                        triggerDetails: null as EmailTrigger | null, // Store trigger details here
+                      })
+                    }
+                  >
+                    Close
+                  </Button>
+                  <div className="w-2" />{' '}
+                  {/* Gap between Close and Edit buttons */}
+                  <Button onClick={handleEditToggle}>Edit</Button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </Drawer>
+
       <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.Head>Name</Table.Head>
-          <Table.Head>Condition Type</Table.Head>
-          <Table.Head>Days Offset</Table.Head>
-          <Table.Head>Created At</Table.Head>
-          <Table.Head></Table.Head>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-
-        {emailTriggers.length === 0 ? (
+        <Table.Header>
           <Table.Row>
-            <Table.Cell colSpan={5} className="text-center">
-              No email triggers found.
-            </Table.Cell>
+            <Table.Head>Name</Table.Head>
+            <Table.Head>Condition Type</Table.Head>
+            <Table.Head>Days Offset</Table.Head>
+            <Table.Head>Created At</Table.Head>
+            <Table.Head></Table.Head>
           </Table.Row>
-
-        ) : (emailTriggers.map((trigger) => (
-            <Table.Row key={trigger.id} className="cursor-pointer">
-              <Table.Cell>{trigger.name}</Table.Cell>
-              <Table.Cell>{getConditionTypeLabel(trigger.condition_type)}</Table.Cell>
-              <Table.Cell>{trigger.days_offset}</Table.Cell>
-              <Table.Cell>{new Date(trigger.created_at).toLocaleString()}</Table.Cell>
-              <Table.Cell>
-                <Link href={`/actions/${trigger.id}`}>
-                  <Button variant="outline" size="sm">Details</Button>
-                </Link>
+        </Table.Header>
+        <Table.Body>
+          {emailTriggers.length === 0 ? (
+            <Table.Row>
+              <Table.Cell colSpan={5} className="text-center">
+                No email triggers found.
               </Table.Cell>
             </Table.Row>
-          ))
-        )}
-      </Table.Body>
-    </Table>
-    
+          ) : (
+            emailTriggers.map((trigger) => (
+              <Table.Row key={trigger.id} className="cursor-pointer">
+                <Table.Cell>{trigger.name}</Table.Cell>
+                <Table.Cell>
+                  {getConditionTypeLabel(trigger.condition_type)}
+                </Table.Cell>
+                <Table.Cell>{trigger.days_offset}</Table.Cell>
+                <Table.Cell>
+                  {new Date(trigger.created_at).toLocaleString()}
+                </Table.Cell>
+                <Table.Cell>
+                  {/* <Link href={`/actions/${trigger.id}`}> */}
+                  {/* <Button variant="outline" size="sm">Details</Button> */}
+                  {/* <Button
+          variant="outline"
+          onClick={() =>
+            setDrawerState((prevState) => ({
+              ...prevState,
+              isOpen: true,
+              size: "lg",
+            }))
+          }
+        >
+          lg
+        </Button> */}
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => fetchEmailTriggerById(trigger.id)} // Open drawer with details
+                  >
+                    Details
+                  </Button>
+
+                  {/* </Link> */}
+                </Table.Cell>
+              </Table.Row>
+            ))
+          )}
+        </Table.Body>
+      </Table>
     </>
   );
 }
